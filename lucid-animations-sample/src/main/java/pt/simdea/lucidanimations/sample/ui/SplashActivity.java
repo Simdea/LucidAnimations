2e7b391da2affe04c91bc44ae10eafaeed8b5462
Paulo <paulo.ribeiro@simdea.pt>
2017-10-22 15:56:32 +0100

    [New] Initial commit.

---
+++ lucid-animations-sample/src/main/java/pt/simdea/lucidanimations/sample/ui/SplashActivity.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2017. Simdea.
+ */
+
+package pt.simdea.lucidanimations.sample.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Class responsible for the Splash Screen screen.
+ *
+ * Created by Paulo Ribeiro on 10/22/2017.
+ * Simdea © All Rights Reserved.
+ * paulo.ribeiro@simdea.pt
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    /**
+     * Called when SplashActivity is first created.
+     * @param savedInstanceState Bundle object containing the activity's previously saved state.
+     */
+    @Override
+    protected void onCreate(@Nullable final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SampleActivity.start(this); // Start home activity
+        finish(); // close splash activity
+    }
+
+}
